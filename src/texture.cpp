5c9f72dd542064463b6d86b5fd4a705fa91fe55e
Sorin - Andrei Ciuntu <s.a.ciuntu@student.tudelft.nl>
2023-10-31 20:38:39 +0100

    Fixed sampleNearestTexture not maintaining texture orientation

--- src/texture.cpp
+++ src/texture.cpp
@@ -6,7 +6,7 @@ glm::vec3 getPixelFromCoordinates(const Image& image, const int xCoord, const in
 {
     // Extracts the pixel at (xCoord, yCoord) from the 1D array of the image
 
-    return image.pixels[image.pixels.size() - (yCoord * image.width + xCoord) - 1];
+    return image.pixels[(image.height - yCoord - 1) * image.width + xCoord];
 }
 
 // TODO: Standard feature
@@ -26,8 +26,12 @@ glm::vec3 sampleTextureNearest(const Image& image, const glm::vec2& texCoord)
     // The pixel are stored in a 1D array of row major order
     // you can convert from position (i,j) to an index using the method seen in the lecture
     // Note, the center of the first pixel is at image coordinates (0.5, 0.5)
-    int xCoord = glm::round(texCoord[0] * (image.width - 1));
-    int yCoord = glm::round(texCoord[1] * (image.height - 1));
+    int xCoord = glm::floor(texCoord[0] * image.width);
+    if (xCoord >= image.width)
+        xCoord = image.width - 1;
+    int yCoord = glm::floor(texCoord[1] * image.height);
+    if (yCoord >= image.height)
+        yCoord = image.height - 1;
     
     return getPixelFromCoordinates(image, xCoord, yCoord);
 }
@@ -52,24 +56,76 @@ glm::vec3 sampleTextureBilinear(const Image& image, const glm::vec2& texCoord)
 
 
     // Points to interpolate from: TL = top left, TR = top right, BL = bottom left, BR = bottom right
-    int xCoordTL = glm::floor(texCoord[0] * (image.width - 1));
-    int yCoordTL = glm::ceil(texCoord[1] * (image.height - 1));
+    int xCoordTL, yCoordTL, xCoordBL, yCoordBL, xCoordTR, yCoordTR, xCoordBR, yCoordBR;
 
-    int xCoordTR = glm::ceil(texCoord[0] * (image.width - 1));
-    int yCoordTR = glm::ceil(texCoord[1] * (image.height - 1));
+    xCoordTL = glm::floor(texCoord[0] * image.width);
+    if (texCoord[0] - (int)texCoord[0] < 0.5f)
+        --xCoordTL;
+    if (xCoordTL < 0)
+        xCoordTL = 0;
+    if (xCoordTL >= image.width)
+        xCoordTL = image.width - 1;
+    xCoordBL = xCoordTL;
+    xCoordTR = xCoordTL + 1;
+    if (xCoordTR >= image.width)
+        xCoordTR = xCoordTL;
+    xCoordBR = xCoordTR;
 
-    int xCoordBL = glm::floor(texCoord[0] * (image.width - 1));
-    int yCoordBL = glm::floor(texCoord[1] * (image.height - 1));
 
-    int xCoordBR = glm::ceil(texCoord[0] * (image.width - 1));
-    int yCoordBR = glm::floor(texCoord[1] * (image.height - 1));
+    yCoordTL = glm::floor(texCoord[1] * image.height);
+    if (texCoord[1] - (int)texCoord[1] < 0.5f)
+        --yCoordTL;
+    if (yCoordTL < 0)
+        yCoordTL = 0;
+    if (yCoordTL >= image.height)
+        yCoordTL = image.height - 1;
+    yCoordTR = yCoordTL;
+    yCoordBL = yCoordTL + 1;
+    if (yCoordBL >= image.height)
+        yCoordBL = image.height - 1;
+    yCoordBR = yCoordBL;
+
+    /* OLD, INCORRECT IMPLEMENTATION:
+    int xCoordTL = glm::floor(texCoord[0] * image.width);
+    if (xCoordTL > image.width)
+        xCoordTL = image.width - 1;
+    int yCoordTL = glm::ceil(texCoord[1] * image.height);
+    if (yCoordTL > image.height)
+        yCoordTL = image.height - 1;
+
+    int xCoordTR = glm::ceil(texCoord[0] * image.width);
+    if (xCoordTR > image.width)
+        xCoordTR = image.width - 1;
+    int yCoordTR = glm::ceil(texCoord[1] * image.height);
+    if (yCoordTR > image.height)
+        yCoordTR = image.height - 1;
+
+    int xCoordBL = glm::floor(texCoord[0] * image.width);
+    if (xCoordBL > image.width)
+        xCoordBL = image.width - 1;
+    int yCoordBL = glm::floor(texCoord[1] * image.height);
+    if (yCoordBL > image.height)
+        yCoordBL = image.height - 1;
+
+    int xCoordBR = glm::ceil(texCoord[0] * image.width);
+    if (xCoordBR > image.width)
+        xCoordBR = image.width - 1;
+    int yCoordBR = glm::floor(texCoord[1] * image.height);
+    if (yCoordBR > image.height)
+        yCoordBR = image.height - 1;
+    */
 
     // Coordinates of the point we interpolate for
-    float xCoord = texCoord[0] * (image.width - 1);
-    float yCoord = texCoord[1] * (image.height - 1);
     
-    float alpha = xCoord - xCoordBL;
-    float beta = yCoord - yCoordBL;
+    float xCoord = texCoord[0] * image.width;
+    if (xCoord >= image.width)
+        xCoord = image.width - 1;
+    float yCoord = texCoord[1] * image.height;
+    if (yCoord >= image.height)
+        yCoord = image.height - 1;
+    
+    float alpha = xCoord - xCoordBL + 0.5f;
+    float beta = yCoord - yCoordBL + 0.5f;
 
     // Compute areas
     float coefTL = (1.f - alpha) * beta;
